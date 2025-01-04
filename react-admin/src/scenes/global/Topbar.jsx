f5c898d00b634dc8fe3df519e10a633c13332dcd
afi <afeefkm95@gmail.com>
2025-01-04 20:14:17 +0530

    Added React-admin

---
+++ react-admin/src/scenes/global/Topbar.jsx
@@ -0,0 +1,39 @@
+import { Box, IconButton, useTheme } from "@mui/material";
+import { useContext } from "react";
+import { ColorModeContext, tokens } from "../../theme";
+import InputBase from "@mui/material/InputBase";
+import LightModeOutlinedIcon from "@mui/icons-material/LightModeOutlined";
+import DarkModeOutlinedIcon from "@mui/icons-material/DarkModeOutlined";
+import SearchOutlinedIcon from "@mui/icons-material/SearchOutlined";
+
+const Topbar = () => {
+  const theme = useTheme();
+  const colors = tokens(theme.palette.mode);
+  const colorMode = useContext(ColorModeContext);
+
+  return (
+    <Box display="flex" justifyContent="space-between" p={2}>
+      <Box
+        display="flex"
+        backgroundColor={colors.primary[400]}
+        borderRadius="3px"
+      >
+        <InputBase sx={{ ml: 2, flex: 1 }} placeholder="Search"></InputBase>
+        <IconButton type="button" sx={{ p: 1 }}>
+          <SearchOutlinedIcon />
+        </IconButton>
+      </Box>
+      <Box display="flex">
+        <IconButton type="button" onClick={colorMode.toggleColorButton}>
+          {theme.palette.mode === "dark" ? (
+            <DarkModeOutlinedIcon />
+          ) : (
+            <LightModeOutlinedIcon />
+          )}
+        </IconButton>
+      </Box>
+    </Box>
+  );
+};
+
+export default Topbar;
